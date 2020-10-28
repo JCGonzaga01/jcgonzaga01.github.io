5a5f8e211c4282cd59b25cad5c7f354917beb996
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-10-29 00:39:43 +0800

    List of changes: 1. Updated banner 2. Updated Banner component text and design

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,23 @@
 import jcgHeader from "./jcg_header.png";
 import jcgHeaderHover from "./jcg_header_hover.png";
 import bannerImg from "./banner.jpg";
+import biking from "./svg/biking.svg";
+import running from "./svg/running.svg";
+import musicNote1 from "./svg/music-note1.svg";
+import guitar from "./svg/guitar.svg";
+import keyboard from "./svg/keyboard.svg";
+import cajon from "./svg/cajon.svg";
+import drums from "./svg/drums.svg";
 
 export default {
   jcgHeader,
   jcgHeaderHover,
   bannerImg,
+  biking,
+  running,
+  musicNote1,
+  guitar,
+  keyboard,
+  cajon,
+  drums,
 };
--- src/pages/Banner/index.tsx
+++ src/pages/Banner/index.tsx
@@ -5,22 +5,67 @@ import styles from "./Banner.scss";
 
 const Banner: React.FC = () => {
   const deviceType = useDeviceType();
+
+  const handleGetStartedClick = () => {};
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div
+        {/* <div
           className={styles.bannerImg}
           style={{
             background: `url(${assets.bannerImg}) center center / cover no-repeat`,
             // Parallex is not supported in tl and sp
             backgroundAttachment: deviceType === "pc" ? "fixed" : "initial",
           }}
-        />
+        /> */}
         <div className={styles.bannerTextWrapper}>
           <div className={styles.bannerTextContainer}>
             <div>John Christopher Gonzaga</div>
-            <div>Web Developer</div>
-            <div>Who am I?</div>
+            <div>A Web Developer who loves:</div>
+            <div>
+              <span className={styles.textWithImage}>
+                <img src={assets.biking} alt={"Biking"} />
+                Biking,
+              </span>
+              <span className={styles.textWithImage}>
+                <img src={assets.running} alt={"Running"} />
+                Running,
+              </span>
+              <span className={styles.textWithImage}>
+                &nbsp; &&
+                <img src={assets.musicNote1} alt={"Music Note 1"} />
+                Playing musical instruments
+              </span>
+            </div>
+            {/* <div>
+              <span>Like</span>
+              <span className={styles.textWithImage}>
+                <img src={assets.guitar} alt={"Guitar"} />
+                Guitar,
+              </span>
+              <span className={styles.textWithImage}>
+                <img src={assets.keyboard} alt={"Keyboard"} />
+                Keyboard,
+              </span>
+              <span className={styles.textWithImage}>
+                <img src={assets.cajon} alt={"Cajon"} />
+                Cajon,
+              </span>
+            </div>
+            <div>
+              <span className={styles.textWithImage}>
+                &nbsp; && a little bit of
+                <img src={assets.drums} alt={"Drums"} />
+                drums
+              </span>
+            </div> */}
+            <div>
+              <div onClick={handleGetStartedClick}>
+                <span>Get Started</span>
+                <div />
+              </div>
+            </div>
           </div>
         </div>
       </div>
