3b9e2350907b1b4f4bfd9a713446440ccdbfd21b
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-11-10 16:51:52 +0800

    Initial Contact Component implementation

--- src/pages/Home/components/Contact/index.tsx
+++ src/pages/Home/components/Contact/index.tsx
@@ -1,8 +1,37 @@
-import React, { useRef } from "react";
+import { validate } from "json-schema";
+import React, { FormEvent, useRef, useState } from "react";
 import styles from "./Contact.scss";
 
 const Contact: React.FC = () => {
   const formRef = useRef(null);
+  const [validateEmail, setValidateEmail] = useState({ valid: true, email: "" });
+
+  const handleOnSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+    setValidateEmail({ valid: true, email: "" });
+    const form: any = formRef.current;
+    const emailRx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const checkInvalidEmail = !emailRx.test(form["contactEmail"].value);
+
+    if (checkInvalidEmail) setValidateEmail({ valid: false, email: form["contactEmail"].value });
+    else {
+      const formValues = {
+        name: form["contactName"].value,
+        email: form["contactEmail"].value,
+        message: form["contactMessage"].value,
+      };
+      handleOnClickClear(e);
+
+      console.log(formValues);
+    }
+  };
+
+  const handleOnClickClear = (e: FormEvent) => {
+    e.preventDefault();
+    const form: any = formRef.current;
+    const formfields = ["contactName", "contactEmail", "contactMessage"];
+    formfields.forEach((item) => (form[item].value = ""));
+  };
 
   return (
     <div id={"contactDivId"} className={styles.wrapper}>
@@ -11,20 +40,33 @@ const Contact: React.FC = () => {
           <span>{"Contact"}</span>
         </div>
         <div className={styles.details}>
-          {/* <form ref={formRef}>
-            <div>
-              <div>
-                <span>Name</span>
-                <input type={"text"} placeholder={""} />
+          {!validateEmail.valid && (
+            <div className={styles.invalidForm}>
+              <div>{`'${validateEmail.email}' is not a valid email address.`}</div>
+            </div>
+          )}
+          <form ref={formRef} onSubmit={handleOnSubmitForm}>
+            <div className={styles.nameNemail}>
+              <div className={styles.textField}>
+                <span>{"Name"}</span>
+                <input name={"contactName"} type={"text"} required />
               </div>
-              <div>
-                <span>Email</span>
-                <input type={"email"} placeholder={""} />
+              <div className={styles.textField}>
+                <span>{"Email"}</span>
+                <input name={"contactEmail"} type={"email"} required />
               </div>
             </div>
-            <div>message form here..</div>
-          </form> */}
-          <div className={styles.contactInfo}> Contact Info</div>
+            <div className={styles.message}>
+              <span>{"Message"}</span>
+              <textarea name={"contactMessage"} rows={8} style={{ resize: "none" }} required />
+            </div>
+            <div className={styles.btnContainer}>
+              <button type={"submit"}>{"Send Message"}</button>
+              <button type={"button"} onClick={handleOnClickClear}>
+                {"Clear"}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
